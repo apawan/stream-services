a87b164d835affe5a3a082a77698a824ec7c6fcc
Roman Kniazevych <aboruhen@gmail.com>
2025-10-20 14:12:33 +0300

    config for eph runtime

---
+++ eph/stop-port-forward.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Get the namespace from 1st argument. If empty fail
+
+TC=$(ps -ef | grep 8080:8080| grep "n $1"| awk '{print $2}')
+AC=$(ps -ef | grep 8030:8080| grep "n $1"| awk '{print $2}')
+AM=$(ps -ef | grep 8040:8080| grep "n $1"| awk '{print $2}')
+UM=$(ps -ef | grep 8050:8080| grep "n $1"| awk '{print $2}')
+
+for portforward in $TC $AC $AM $UM; do
+  if [ -n "$portforward" ]; then
+    echo "Port forward $portforward exists, killing it now"
+    kill "$portforward"
+  fi
+done
